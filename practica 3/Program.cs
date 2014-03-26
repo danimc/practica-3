a2c9bb44d33d56ff005e6c781a492927f93b6ff8
dany <daniel_k310a@hotmail.com>
2014-03-25 18:05:42 -0600

    integracion del modificador de llaves

--- practica 3/Program.cs
+++ practica 3/Program.cs
@@ -36,6 +36,7 @@ namespace practica_3
 			}while(a <= 2);
 			
 			PrintKeysAndValues(tabla);
+			
 			Console.WriteLine(" Deseas Modificar[1] o eliminar[2] un valor");
 			int eleccion = int.Parse(Console.ReadLine());
 			Console.Clear();
@@ -44,9 +45,29 @@ namespace practica_3
 				
 				Console.WriteLine("Captura la clave a modificar");
 				clave_elegida = Console.ReadLine();
+				Console.Clear();
 				if(tabla.Contains(clave_elegida)){
-					Console.WriteLine(tabla[clave_elegida].ToString());
+					Console.WriteLine("¿Seguro que quiere modificar este registro?");
+					Console.WriteLine("Clave  Nombre");
+					Console.WriteLine(clave_elegida + ' ' +  tabla[clave_elegida].ToString());
+					char seguridad = char.Parse(Console.ReadLine());
+					if (seguridad == 's'){
+						persona p = new persona();
+						Console.WriteLine("Nuevo nombre:");
+						p.nombre = Console.ReadLine();
+						Console.WriteLine("Nueva Clave");
+						p.clave = Console.ReadLine();
+						tabla.Remove(clave_elegida);
+						tabla.Add(p.clave, p.nombre);
+						Console.WriteLine("la tabla se ah modificado");
+						}
+					else{
+						Console.WriteLine("No se ah hecho ningun cambio");
+						}
 					}
+				else{
+					Console.WriteLine("La Clave ingresada no existe");
+				}
 				
 			}
 			
@@ -62,7 +83,7 @@ namespace practica_3
 		
 		
 	      public static void PrintKeysAndValues( Hashtable tabla )  {
-    	  Console.WriteLine( "\t-Clave-\t-Nombre-" );
+    	  Console.WriteLine( "\tClave \t Nombre " );
     	  foreach ( DictionaryEntry de in tabla )
           Console.WriteLine( "\t{0}:\t{1}", de.Key, de.Value );
     	  Console.WriteLine();
